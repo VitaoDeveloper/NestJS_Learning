4bf2a69e0cc42d5d7ddf7e7102bbbf3c29e136c5
VitaoDeveloper <paulo.borges.prates@gmail.com>
2026-05-29 13:08:58 -0300

    Atualizando modules

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { GenresModule } from '../genres/genres.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Book]),
+    GenresModule, // ← importa para ter acesso ao GenresService
+  ],
   controllers: [BooksController],
-  providers: [BooksService],
+  providers:   [BooksService],
 })
 export class BooksModule {}
